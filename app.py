fa979c92dfa809da25e2a1a5f0b7b0069cc372d1
nicohman <nicohman@demenses.net>
2019-03-06 11:46:59 -0800

    add extra links to header bar

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask,send_from_directory,send_file,abort
 import os
+from export import export
+export()
 app = Flask(__name__)
 @app.route("/")
 def index():
@@ -13,5 +15,8 @@ def serve(path):
         return send_from_directory("export",path + ".html")
     else:
         abort(404)
+@app.errorhandler(404)
+def not_found():
+    return "The path you specified could not be found."
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port = 4000)
--- export.py
+++ export.py
@@ -1,9 +1,16 @@
 from pyquery import PyQuery as pq
 from org_sourcehut.convert import convert
 import os
+import json
 def is_html(name):
     return ".html" in name
-os.system("emacs --batch -l export.el -f export")
-pages = os.listdir("export")
-for page in filter(is_html,pages):
-    convert("export/"+page)
+def export():
+    fd = open("config.json", "r")
+    config = json.load(fd)
+    links = config['links']
+    os.system("emacs --batch -l export.el -f export")
+    pages = os.listdir("export")
+    for page in filter(is_html,pages):
+        convert("export/"+page, links=links, show_up_link=False)
+if __name__ == "__main__":
+    export()
