42db7c597363252e2e4f2214953ea377faf46796
nicohman <nicohman@demenses.net>
2019-03-05 16:47:36 -0800

    Add gitignore and first version of flask server

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask,send_from_directory,send_file
+import os
+app = Flask(__name__)
+@app.route("/")
+def index():
+    return send_file("export/index.html")
+@app.route("/<path:path>")
+def serve(path):
+    files = os.listdir("export")
+    if path in files:
+        return send_from_directory("export",path)
+    elif path + ".html" in files:
+        return send_from_directory("export",path + ".html")
+    else:
+        abort(404)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port = 4000)
